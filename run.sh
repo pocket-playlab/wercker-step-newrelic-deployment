8e414f0faa8002dfb9a47ae3ffd49c974f040661
Rafael Verger <rafael@rafaelverger.com.br>
2014-04-16 20:38:50 -0300

    Initial version: sends deploy event with api_key to app_name with revision message

---
+++ run.sh
@@ -0,0 +1,15 @@
+if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_API_KEY" ]; then
+  error 'Please specify api_key property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME" ]; then
+  error 'Please specify app_name property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_REVISION" ]; then
+  export WERCKER_NEWRELIC_DEPLOYMENT_REVISION="$WERCKER_GIT_BRANCH/${WERCKER_GIT_COMMIT:0:10} by $WERCKER_STARTED_BY"
+fi
+
+curl -H "x-license-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" https://api.newrelic.com/deployments.xml
