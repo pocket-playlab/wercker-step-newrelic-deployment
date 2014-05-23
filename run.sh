c1b82201519fdf0c8fca339fdccbf0b76ca29225
MacTynow <charles.martinot@gmail.com>
2014-05-23 11:54:09 +0700

    Update run.sh
    Added set -e so exits are more useful

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_API_KEY" ]; then
   error 'Please specify api_key property'
   exit 1
@@ -12,4 +16,5 @@ if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_REVISION" ]; then
   export WERCKER_NEWRELIC_DEPLOYMENT_REVISION="$WERCKER_GIT_BRANCH/${WERCKER_GIT_COMMIT:0:10} by $WERCKER_STARTED_BY"
 fi
 
+echo "sending request : curl -H 'x-license-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}' -d 'deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}' -d 'deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}' https://api.newrelic.com/deployments.xml"
 curl -H "x-license-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" https://api.newrelic.com/deployments.xml
