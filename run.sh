d2f146ec8f81f577f1dde75b447048a2232a6cef
MacTynow <charles.martinot@gmail.com>
2014-05-23 12:08:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,5 +16,4 @@ if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_REVISION" ]; then
   export WERCKER_NEWRELIC_DEPLOYMENT_REVISION="$WERCKER_GIT_BRANCH/${WERCKER_GIT_COMMIT:0:10} by $WERCKER_STARTED_BY"
 fi
 
-echo "sending request : curl -H 'x-license-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}' -d 'deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}' -d 'deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}' https://api.newrelic.com/deployments.xml"
-curl -H "x-license-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" https://api.newrelic.com/deployments.xml
+curl -H "x-api-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" https://api.newrelic.com/deployments.xml
